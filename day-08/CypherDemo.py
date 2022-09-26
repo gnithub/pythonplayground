1ef298a81261f9c3e76337ddb602f351b10410b0
Nitin Upadhyay <er.upadhyaynitin@gmail.com>
2022-09-26 10:40:01 +0530

    Final version cypher demo.

--- day-08/CypherDemo.py
+++ day-08/CypherDemo.py
@@ -1,7 +1,10 @@
+from logoText import logo
+
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 def encode(char, shift):
-    print(f"Char is {char} with shift {shift}")
+    print(f"Char is {char} with shift {shift} and direction is {direction}")
+
     if char == ' ':
         return char
     return alphabet[alphabet.index(char)+shift]
@@ -13,17 +16,30 @@ def decode(char, shift):
     return alphabet[alphabet.index(char)-shift]
 
 
-text=input("Input a text: ").lower()
-shift=int(input("Input shift: "))
-direction=input("Direction : ")
+choice = True
+while choice:
+    print(logo)
+    text=input("Input a text: ").lower()
+    shift=int(input("Input shift: "))
+    direction=input("Direction : ")
+    shift = shift % 26
 
-final_text=""
-if direction == "encode":
-    for ch in text:
-        final_text += encode(ch, shift)
-elif direction == "decode":
-    for ch in text:
-        final_text += decode(ch, shift)
+    print(shift)
+    print(len(alphabet))
 
-print(f"Final word is {final_text}")
+    final_text=""
+    if direction == "encode":
+        for ch in text:
+            final_text += encode(ch, shift)
+    elif direction == "decode":
+        for ch in text:
+            final_text += decode(ch, shift)
+
+    print(f"Final word is {final_text}")
+
+    yesno = input("Want to execute again? ").lower()
+    if yesno == "no" :
+        print("good bye...")
+        choice = False
+
 
